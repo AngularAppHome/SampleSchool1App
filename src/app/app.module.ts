7aa7a35999b3f905a1240264fc0c8e54df4885ba
vipin-cutting-edge <vipin.hari@outlook.com>
2018-03-17 14:41:25 +0530

    pages are added with navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,49 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { StudentComponent } from './student/student.component';
+import { TeacherComponent } from './teacher/teacher.component';
+import { SchoolbusComponent } from './schoolbus/schoolbus.component';
+import { StaffComponent } from './staff/staff.component';
+import { AssetComponent } from './asset/asset.component';
+import { DashboardComponent } from 'app/dashboard/dashboard.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+
+const appRoutes: Routes = [
+  { path: 'student', component: StudentComponent },
+  { path: 'teacher',      component: TeacherComponent },
+  { path: 'schoolbus',      component: SchoolbusComponent },
+  { path: 'staff',      component: StaffComponent },
+  { path: 'asset',      component: AssetComponent },
+  { path: 'dashboard',      component: DashboardComponent },
+  { path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentComponent,
+    TeacherComponent,
+    SchoolbusComponent,
+    StaffComponent,
+    AssetComponent,
+    DashboardComponent,
+    PageNotFoundComponent
   ],
   imports: [
+      RouterModule.forRoot(
+        appRoutes,
+        { enableTracing: true } // <-- debugging purposes only
+      ),
     BrowserModule,
     FormsModule,
     HttpModule
---
+++ src/app/schoolbus/schoolbus.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-schoolbus',
+  templateUrl: './schoolbus.component.html',
+  styleUrls: ['./schoolbus.component.css']
+})
+export class SchoolbusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
